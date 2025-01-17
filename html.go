a34d6024325a10cefa21cfd4b991dc2154145a43
user <user@mail.com>
2025-01-17 00:00:07 -0500

    check in stuff

--- html.go
+++ html.go
@@ -72,7 +72,7 @@ func createHTMLTemplate(title, description, body string) string {
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=MedievalSharp&family=Crimson+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     
     <!-- Syntax highlighting -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css">
